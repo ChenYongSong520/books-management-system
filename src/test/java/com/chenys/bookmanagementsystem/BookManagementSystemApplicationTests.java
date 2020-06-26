4489a0b840de27d7535224603c256b38ddad16c8
cys297 <chenyongsong0@163.com>
2020-06-26 21:13:11 +0800

    初始化仓库

---
+++ src/test/java/com/chenys/bookmanagementsystem/BookManagementSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.chenys.bookmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}
